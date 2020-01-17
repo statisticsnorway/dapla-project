de566b6c2b455119d47f7b38d9aa044ca9b7ba3a
Kenneth Leine Schulstad <ks@udp.no>
2020-01-17 13:43:33 +0100

    Add script for restarting spark interpreter using Zeppelin's REST API
    Speeds up redeploy. And no more need for user login and tedious
    interpreter seleection when the plugin is redeployed.

---
+++ dapla-localstack/bin/restart-spark-interpreter.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+zeppelin_host=http://localhost:18010
+cookie_file=zeppelin-cookies.txt
+
+printf "Zeppelin login... "
+status=$(curl -X POST -s -c "$cookie_file" -d 'userName=admin&password=password1' "$zeppelin_host/api/login" | jq -r '.status')
+
+if [ "${status}" = "OK" ]
+then
+    printf "${status}\nRestart Zeppelin Spark interpreter... "
+    status=$(curl -X PUT -s -b "$cookie_file" "$zeppelin_host/api/interpreter/setting/restart/spark" | jq -r '.status')
+fi
+printf "${status:-ERROR}\n"
+
+rm -f "$cookie_file"
