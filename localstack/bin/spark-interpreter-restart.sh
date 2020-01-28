380c56476bde55a420681306733137385f6440c5
Kenneth Leine Schulstad <ks@udp.no>
2020-01-28 15:17:15 +0100

    Use zeppelin-api bash functions

--- localstack/bin/spark-interpreter-restart.sh
+++ localstack/bin/spark-interpreter-restart.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env sh
 
-zeppelin_host=http://localhost:28010
-cookie_file=zeppelin-cookies.txt
-
-printf "Zeppelin login... "
-status=$(curl -X POST -s -c "$cookie_file" -d 'userName=admin&password=password1' "$zeppelin_host/api/login" | jq -r '.status')
-
-if [ "${status}" = "OK" ]
-then
-    printf "${status}\nRestart Zeppelin Spark interpreter... "
-    status=$(curl -X PUT -s -b "$cookie_file" "$zeppelin_host/api/interpreter/setting/restart/spark" | jq -r '.status')
-fi
-printf "${status:-ERROR}\n"
-
-rm -f "$cookie_file"
+source $DAPLA_PROJECT_HOME/localstack/bin/zeppelin-api.sh
+zeppelin_login admin password1
+printf "Restarting Spark interpreter... "
+zeppelin_put "/api/interpreter/setting/restart/spark" > /dev/null
+printf "$(green OK)\n"
+zeppelin_logout
