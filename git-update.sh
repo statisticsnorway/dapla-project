fc1210a976e0f1d5ef74d129f2fbc86837c86b70
Kenneth Leine Schulstad <ks@udp.no>
2020-02-05 15:22:19 +0100

    Improve status handling

--- git-update.sh
+++ git-update.sh
@@ -3,22 +3,21 @@
 function update {
     printf "Updating $1$2... "
     output=$(git -C ${1}/ pull --rebase 2>&1)
-    print_status
+    handle_response
 }
 
 function clone {
     printf "Cloning $1... "
     output=$(git clone git@github.com:statisticsnorway/${1}.git 2>&1)
-    print_status
+    handle_response
 }
 
-function print_status {
+function handle_response {
     if [ $? -eq 0 ]
     then
         printf '\e[32m%s\e[0m\n' OK
     else
-        printf '\e[31m%s\e[0m\n' ERROR
-        echo $output
+        printf '\e[31m%s\e[0m\n%s\n\n' ERROR "$output"
     fi
 }
 
