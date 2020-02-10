1491ca76b47c9d513ab281e5f558c9ca87c0c1f4
Kenneth Leine Schulstad <ks@udp.no>
2020-02-10 09:53:34 +0100

    Use separate database user for secret service

--- localstack/docker/postgres/init-db.sql
+++ localstack/docker/postgres/init-db.sql
@@ -4,3 +4,8 @@
 CREATE USER rdc WITH PASSWORD 'rdc';
 CREATE DATABASE rdc;
 GRANT ALL PRIVILEGES ON DATABASE rdc TO rdc;
+
+-- Create user and database
+CREATE USER secret_service WITH PASSWORD 'secret_service';
+CREATE DATABASE secret_service;
+GRANT ALL PRIVILEGES ON DATABASE secret_service TO secret_service;
