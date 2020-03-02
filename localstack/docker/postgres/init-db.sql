bb3b6f9dbffd1023affb1b44b6632013d9b8d71c
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-03-02 15:52:33 +0100

    Remove unused secret-service database

--- localstack/docker/postgres/init-db.sql
+++ localstack/docker/postgres/init-db.sql
@@ -5,11 +5,6 @@ CREATE USER dataset_access WITH PASSWORD 'dataset_access';
 CREATE DATABASE dataset_access;
 GRANT ALL PRIVILEGES ON DATABASE dataset_access TO dataset_access;
 
--- Secret Service
-CREATE USER secret_service WITH PASSWORD 'secret_service';
-CREATE DATABASE secret_service;
-GRANT ALL PRIVILEGES ON DATABASE secret_service TO secret_service;
-
 -- Catalog
 CREATE USER catalog WITH PASSWORD 'catalog';
 CREATE DATABASE catalog;
