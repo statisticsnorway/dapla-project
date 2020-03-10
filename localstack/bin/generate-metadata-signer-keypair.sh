d86a510fd823c0c149c545c44514e7dd6ef12ca5
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-03-10 13:33:43 +0100

    Added metadata key generation target and correctly mount folder with keys

--- localstack/bin/generate-metadata-signer-keypair.sh
+++ localstack/bin/generate-metadata-signer-keypair.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+cd $(dirname $BASH_SOURCE)/../..
+mkdir -p localstack/secret
+mkdir -p data-access/secret
+cd localstack/secret
 keytool -genkeypair -alias dataAccessKeyPair -keyalg RSA -keysize 2048 -dname "dc=ssb,dc=no,ou=dapla,cn=data-access" -validity 10000 -storetype PKCS12 -keystore metadata-signer_keystore.p12 -storepass changeit
 keytool -exportcert -alias dataAccessKeyPair -storetype PKCS12 -keystore metadata-signer_keystore.p12 -file metadata-signer_certificate.cer -rfc -storepass changeit
-keytool -importcert -alias dataAccessCertificate -storetype PKCS12 -keystore metadata-verifier_keystore.p12 -file metadata-signer_certificate.cer -rfc -storepass changeit
+keytool -importcert -alias dataAccessCertificate -storetype PKCS12 -keystore metadata-verifier_keystore.p12 -file metadata-signer_certificate.cer -rfc -storepass changeit <<< yes
+cd ../..
+cp localstack/secret/metadata-signer_keystore.p12 data-access/secret
+cp localstack/secret/metadata-verifier_keystore.p12 data-access/secret
