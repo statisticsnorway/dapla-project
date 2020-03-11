8a981cb19413668a4b95374c98cbc7b49879d14d
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-03-11 09:39:09 +0100

    Copy verifier keystore to metadata-distributor and only signer to data-access

--- localstack/bin/generate-metadata-signer-keypair.sh
+++ localstack/bin/generate-metadata-signer-keypair.sh
@@ -3,10 +3,11 @@
 cd $(dirname $BASH_SOURCE)/../..
 mkdir -p localstack/secret
 mkdir -p data-access/secret
+mkdir -p metadata-distributor/secret
 cd localstack/secret
 keytool -genkeypair -alias dataAccessKeyPair -keyalg RSA -keysize 2048 -dname "dc=ssb,dc=no,ou=dapla,cn=data-access" -validity 10000 -storetype PKCS12 -keystore metadata-signer_keystore.p12 -storepass changeit
 keytool -exportcert -alias dataAccessKeyPair -storetype PKCS12 -keystore metadata-signer_keystore.p12 -file metadata-signer_certificate.cer -rfc -storepass changeit
 keytool -importcert -alias dataAccessCertificate -storetype PKCS12 -keystore metadata-verifier_keystore.p12 -file metadata-signer_certificate.cer -rfc -storepass changeit <<< yes
-cd ../..
+cd -
 cp localstack/secret/metadata-signer_keystore.p12 data-access/secret
-cp localstack/secret/metadata-verifier_keystore.p12 data-access/secret
+cp localstack/secret/metadata-verifier_keystore.p12 metadata-distributor/secret
