5da94b85bafe425ba91d5d0643e8e39354350571
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-03-16 14:37:36 +0100

    Added scenario: collection/metadata-ske-sirius-person

---
+++ localstack/bin/scenarios/collection/metadata-ske-sirius-person.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+JSON=$(jq '@json' <<< '{
+  "id": {
+    "path": "/raw/ske/sirius/person/2018",
+    "version": "1584361016731"
+  },
+  "type": "UNBOUNDED",
+  "valuation": "SENSITIVE",
+  "state": "RAW",
+  "parentUri": "gs://bucket/path/that/will/be/replaced",
+  "pseudoConfig": {},
+  "createdBy": "jane-doe"
+}')
+
+mkdir -p tmp
+post $daccess '/rpc/DataAccessService/writeLocation' '{"metadataJson":'$JSON'}' > tmp/write-location.json
+jq -r '.validMetadataJson' tmp/write-location.json | base64 -d > tmp/dataset-meta.json
+jq -r '.metadataSignature' tmp/write-location.json | base64 -d > tmp/dataset-meta.json.sign
+
+echo SIGNED META-DATA
+jq . tmp/dataset-meta.json
+
+DATASET_FOLDER="$(dirname $BASH_SOURCE)/../../../data/datastore$(jq -r '.id.path' tmp/dataset-meta.json)/$(jq -r '.id.version' tmp/dataset-meta.json)"
+
+echo Copying metadata to folder: $DATASET_FOLDER
+
+rm -f $DATASET_FOLDER/.dataset-meta.json
+rm -f $DATASET_FOLDER/.dataset-meta.json.sign
+mkdir -p $DATASET_FOLDER
+cp tmp/dataset-meta.json $DATASET_FOLDER/.dataset-meta.json
+cp tmp/dataset-meta.json.sign $DATASET_FOLDER/.dataset-meta.json.sign
+#rm tmp/dataset-meta.json tmp/dataset-meta.json.sign tmp/write-location.json
+
+post $distributor '/rpc/MetadataDistributorService/dataChanged' '{
+  "projectId": "dapla",
+  "topicName": "file-events-1",
+  "parentUri": '$(jq '.parentUri' tmp/dataset-meta.json)',
+  "path": '$(jq '.id.path' tmp/dataset-meta.json)',
+  "version": '$(jq '.id.version' tmp/dataset-meta.json)',
+  "filename": ".dataset-meta.json.sign"
+}' 200
