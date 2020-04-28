cc2345508590049901c19dd88508e305bf9835c1
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-04-28 16:19:15 +0200

    Fix: Avoid changing directory

--- localstack/bin/generate-concept-schemas.sh
+++ localstack/bin/generate-concept-schemas.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Re-generating schema files..."
-cd docker/concept || exit
-rm -rf jsonschemas
-rm -rf graphqlschemas
+rm -rf docker/concept/jsonschemas
+rm -rf docker/concept/graphqlschemas
 
 docker run -v $(pwd)/docker/concept:/raml-project statisticsnorway/raml-to-jsonschema:latest
 docker run -v $(pwd)/docker/concept:/raml-project statisticsnorway/raml-to-graphql-schema:latest
