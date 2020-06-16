623bab1a9c0c2db7f2c69afb3aa5918017aae4d9
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-06-16 14:17:42 +0200

    Added PST veileder test.

---
+++ localstack/bin/scenarios/prod/pst-veileder-test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+put $auth '/group/pst-veileder-test' '{
+  "groupId": "pst-veileder-test",
+  "description": "Brukere som skal teste PST sin veileder. JIRA: SKATT-216",
+  "roles": ["felles"]
+}' 201
+
+for user in "Christine.Rokkan" \
+  "Anders.Whist" \
+  "Inge.Aukrust" \
+  "Christian.Brovold" \
+  "Arve.Hetland" \
+  "erik.h.nymoen" \
+  "Elisabeth.Omholt"; do
+
+  put $auth "/role/user.$user" '{
+    "roleId": "user.'$user'",
+    "description": "Personlig rolle for '$user'",
+    "paths": {
+      "includes": ["/user/'$user'/"]
+    },
+    "maxValuation": "SENSITIVE",
+  }' 201
+
+  put $auth "/user/$user@ssb.no" '{
+    "userId": "'$user'@ssb.no",
+    "roles": ["user.'$user'"],
+    "groups": ["pst-veileder-test"]
+  }' 201
+done
