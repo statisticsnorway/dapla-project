a4b3757127c78003e5c0f90ce36316fbb980266c
fnh-ssb <fnh@ssb.no>
2020-11-25 22:06:34 +0100

    update

--- localstack/bin/scenarios/prod/vof-test.sh
+++ localstack/bin/scenarios/prod/vof-test.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-put $auth '/group/vof' '{
-  "groupId": "vof",
+put $auth '/group/felles' '{
+  "groupId": "felles",
   "description": "Brukere som skal teste enhetsregisteret data fra brreg.no",
   "roles": ["felles"]
 }' 201
 
-for user in "fenglin.han" \
-  "sjur.sagen" \
-  "simen.sommer" \
-  "mehran.raja" \
-  "audun.rusti" \
-  "anne.harbosen" \
-  "bjorn.skaar"; do
+for user in "fenglin" \
+  "sjur" \
+  "simen" \
+  "mehran" \
+  "audun" \
+  "anne" \
+  "bjorn"; do
 
   put $auth "/role/user.$user" '{
     "roleId": "user.'$user'",
@@ -26,6 +26,6 @@ for user in "fenglin.han" \
   put $auth "/user/$user@ssb.no" '{
     "userId": "'$user'@ssb.no",
     "roles": ["user.'$user'"],
-    "groups": ["vof"]
+    "groups": ["felles"]
   }' 201
 done
